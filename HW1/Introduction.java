81e3109e57067651f3dd50a894386a39222cc741
Phong Nguyen <24020269@vnu.edu.vn>
2025-09-24 21:45:12 +0700

    submit HW1 B

---
+++ HW1/Introduction.java
@@ -0,0 +1,17 @@
+public class Introduction {
+    public static void main(String[] args) {
+        String name = "Nguyễn Duy Phong";
+        String msv = "24020269";
+        String lop = "INT2204_4";
+        String username = "dphongn";
+        String mail = "24020269@vnu.edu.vn";
+        System.out.println(name + "\t" + msv + "\t" + lop +"\t" + username +"\t" + mail);
+        for (int i = 9; i > 1; i--) {
+            System.out.println(i + " bottles of beer on the wall, "+ i + " bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+        }
+        System.out.println("1 bottle of beer on the wall, 1 bottle of beer.");
+        System.out.println("Take one down, pass it around,");
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
