0e8c94f8f883f0630556488222e6891390622caf
Phong Nguyen <24020269@vnu.edu.vn>
2025-10-02 21:58:44 +0700

    Submit HW2

--- HW2/src/main/java/CharGrid.java
+++ HW2/src/main/java/CharGrid.java
@@ -3,32 +3,92 @@
 // a few operations on the grid.
 
 public class CharGrid {
-	private char[][] grid;
+    private char[][] grid;
 
-	/**
-	 * Constructs a new CharGrid with the given grid.
-	 * Does not make a copy.
-	 * @param grid
-	 */
-	public CharGrid(char[][] grid) {
-		this.grid = grid;
-	}
-	
-	/**
-	 * Returns the area for the given char in the grid. (see handout).
-	 * @param ch char to look for
-	 * @return area for given char
-	 */
-	public int charArea(char ch) {
-		return 0; // YOUR CODE HERE
-	}
-	
-	/**
-	 * Returns the count of '+' figures in the grid (see handout).
-	 * @return number of + in grid
-	 */
-	public int countPlus() {
-		return 0; // YOUR CODE HERE
-	}
-	
+    /**
+     * Constructs a new CharGrid with the given grid.
+     * Does not make a copy.
+     * @param grid
+     */
+    public CharGrid(char[][] grid) {
+        this.grid = grid;
+    }
+
+    /**
+     * Returns the area for the given char in the grid. (see handout).
+     * @param ch char to look for
+     * @return area for given char
+     */
+    public int charArea(char ch) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        int minRow = rows, maxRow = -1;
+        int minCol = cols, maxCol = -1;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == ch) {
+                    if (i < minRow) minRow = i;
+                    if (i > maxRow) maxRow = i;
+                    if (j < minCol) minCol = j;
+                    if (j > maxCol) maxCol = j;
+
+                }
+            }
+        }
+        if (maxRow == -1) return 0;
+        return (maxRow - minRow + 1) * (maxCol - minCol + 1); // YOUR CODE HERE
+    }
+
+    /**
+     * Returns the count of '+' figures in the grid (see handout).
+     * @return number of + in grid
+     */
+    public int countPlus() {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int count = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                char ch = grid[i][j];
+                int armLen = armLength(i,j,ch);
+                if (armLen >= 2) {
+                    count++;
+                }
+            }
+        }
+        return count; // YOUR CODE HERE
+    }
+
+    /**
+     * Tính độ dài nhánh chữ thập với tâm (r,c) và ký tự ch.
+     * Trả về chiều dài nhánh nếu 4 nhánh bằng nhau, ngược lại trả về -1.
+     */
+    private int armLength(int r, int c, char ch) {
+        if (c >= grid[r].length || grid[r][c] != ch) return -1;
+
+        int up = 0, down = 0, left = 0, right = 0;
+
+        // lên
+        for (int i = r - 1; i >= 0; i--) {
+            if (c < grid[i].length && grid[i][c] == ch) up++;
+            else break;
+        }
+        // xuống
+        for (int i = r + 1; i < grid.length; i++) {
+            if (c < grid[i].length && grid[i][c] == ch) down++;
+            else break;
+        }
+        // trái
+        for (int j = c - 1; j >= 0 && grid[r][j] == ch; j--) left++;
+        // phải
+        for (int j = c + 1; j < grid[r].length && grid[r][j] == ch; j++) right++;
+
+        if (up >= 1 && up == down && up == left && up == right) {
+            return up;
+        }
+        return -1;
+    }
+
 }
--- HW2/src/main/java/TetrisGrid.java
+++ HW2/src/main/java/TetrisGrid.java
@@ -3,27 +3,54 @@
 // a clearRows() capability.
 
 public class TetrisGrid {
-	
-	/**
-	 * Constructs a new instance with the given grid.
-	 * Does not make a copy.
-	 * @param grid
-	 */
-	public TetrisGrid(boolean[][] grid) {
-	}
-	
-	
-	/**
-	 * Does row-clearing on the grid (see handout).
-	 */
-	public void clearRows() {
-	}
-	
-	/**
-	 * Returns the internal 2d grid array.
-	 * @return 2d grid array
-	 */
-	boolean[][] getGrid() {
-		return null; // YOUR CODE HERE
-	}
+    private boolean [][]grid;
+    /**
+     * Constructs a new instance with the given grid.
+     * Does not make a copy.
+     * @param grid
+     */
+    public TetrisGrid(boolean[][] grid) {
+        this.grid = grid;
+    }
+
+
+    /**
+     * Does row-clearing on the grid (see handout).
+     */
+    public void clearRows() {
+        int width = grid.length;
+        int height = grid[0].length;
+
+        for (int y = 0; y < height; y++) {
+            if(isFullRow(y,width)) {
+                clearRow(y,width,height);
+                y--;
+            }
+        }
+    }
+    private boolean isFullRow(int y, int width) {
+        for (int x = 0; x < width ; x++) {
+            if(!grid[x][y]) return false;
+        }
+        return true;
+    }
+
+    private void clearRow(int row, int width, int height) {
+        for (int y = row; y < height - 1; y++) {
+            for (int x = 0; x < width; x++) {
+                grid[x][y] = grid[x][y + 1];
+            }
+        }
+        for (int x = 0; x < width; x++) {
+            grid[x][height - 1] = false;
+        }
+    }
+    /**
+     * Returns the internal 2d grid array.
+     * @return 2d grid array
+     */
+    boolean[][] getGrid() {
+
+        return grid; // YOUR CODE HERE
+    }
 }
